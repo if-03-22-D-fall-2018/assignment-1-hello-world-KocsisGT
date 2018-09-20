cfa7ddb3b7c4a8410918ca52a7e1ccb8f7c8fa2f
KocsisGT <kocsispatrikistvan@gmail.com>
2018-09-20 11:32:53 +0200

    Stub is working

--- hello_world.c
+++ hello_world.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2DHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:			Kocsis Patrik
+ * ----------------------------------------------------------
+ * Description:
+ * Prints the unavoidable "Hello World" into the console
+ * ----------------------------------------------------------
+ */
+ #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
   printf("Hello World\n");
